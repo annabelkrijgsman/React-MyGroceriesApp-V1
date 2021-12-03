716910e3b1dc62a62bea928ccd00fd3e1d3076ec
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2021-12-03 22:50:15 +0100

    Added h1's and extra styling

--- src/Components/GroceryList.js
+++ src/Components/GroceryList.js
@@ -5,6 +5,7 @@ import InputField from './InputField'
 function GroceryList(items) {
     return (
         <div className='grocery-list'>
+            <h1>Grocerylist</h1>
             <InputField 
                 addNewItem={items.addNewItem}
             />
--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -4,6 +4,7 @@ import List from './List'
 function ShoppingCart(items) {
     return (
         <div className='shopping-cart'>
+            <h1>Shoppingcart</h1>
             <button onClick={items.emptyShoppingCart}>Empty shoppingcart</button>
             <List item={items.item} type='shoppingCart'/>
         </div>
